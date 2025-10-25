81e8b4413c82ea6619646bbe7158f1c4e7533539
unknown <st13vv@gmail.com>
2025-10-25 20:20:28 +0300

    added Hero component

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import type { Route } from './+types/index';
+import Hero from '~/components/Hero';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <section>"My App"</section>;
+  return (
+    <section>
+      <Hero />
+    </section>
+  );
 }
