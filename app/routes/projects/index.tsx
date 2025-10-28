2c3a1328d7e07dd6e5949899826d1b6e6c23bbdd
unknown <st13vv@gmail.com>
2025-10-28 19:38:25 +0200

    added fetch with loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,16 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  console.log(projects);
+
   return <h1 className="text-white text-3xl font-bold">My Projetcs</h1>;
 };
 
