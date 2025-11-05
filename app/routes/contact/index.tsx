c7806025a76ec562df5f51ec422f0a5af6219b96
unknown <st13vv@gmail.com>
2025-11-05 14:04:25 +0200

    added contact form

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,5 +1,72 @@
+import { div } from 'framer-motion/client';
+
 const ContactPage = () => {
-  return <h2 className="text-3xl text-white font-bold"> 🤝 Contact Me</h2>;
+  return (
+    <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
+      <h2 className="text-3xl text-white font-bold"> 🤝 Contact Me</h2>
+
+      <form className="space-y-6">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font font-medium text-gray-300"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font font-medium text-gray-300"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subject"
+            className="block text-sm font font-medium text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            type="subject"
+            id="subject"
+            name="subject"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font font-medium text-gray-300"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 text-gray-100"
+          />
+        </div>
+        <button className="w-full bg-blue-600 text0white py-2 rounded-lg bg-blue-600 hover:bg-blue-700 cursor-pointer">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactPage;
